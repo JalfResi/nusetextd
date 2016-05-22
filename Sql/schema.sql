cf5baac8e10b5bcfd01e03bdecc93d65f50930ff
Ben Davies <omarshariffdontlikeit@gmail.com>
2016-05-22 20:38:35 +0100

    Added sql schema

---
+++ Sql/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS articles (
+    hash BINARY(16) NOT NULL,
+    url TEXT,
+    PRIMARY KEY (hash)
+);
+
+CREATE TABLE IF NOT EXISTS topics (
+    hash BINARY(16) NOT NULL,
+    label TINYTEXT,
+    PRIMARY KEY (hash)    
+);
+
+CREATE TABLE IF NOT EXISTS article_has_topics (
+    articleHash BINARY(16) NOT NULL,
+    topicHash BINARY(16) NOT NULL,
+    PRIMARY KEY (articleHash, topicHash),
+    FOREIGN KEY (articleHash) REFERENCES articles(hash),
+    FOREIGN KEY (topicHash) REFERENCES topics(hash)    
+);
+
